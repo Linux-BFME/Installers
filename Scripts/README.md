7788eba7f8f7e5cec13121bb5cfc3a1ec643a165
dginovker <dcress01@uoguelph.ca>
2021-01-25 18:15:07 -0500

    script documentation

---
+++ Scripts/README.md
@@ -0,0 +1,5 @@
+BFME Gotchas:
+   1. Two games with the same CD Key will get a "SERIAL ALREADY IN USE" error when joining a multiplayer game
+   2. Changing registry "HKEY_LOCAL_MACHINE\Software\Wow6432Node\Electronic Arts\EA Games\The Battle for Middle-earth\ergc" to fix that gives 3 minute auto-defeat
+
+The solution (outlined https://www.gamereplays.org/community/index.php?showtopic=1010428) is to make copies of system.reg and LOTRBFMe.dat and give them to players. That's what these scripts do :)
--- Scripts/bfme1/bfme1.sh
+++ Scripts/bfme1/bfme1.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# BFME1 Gotchas:
-#   1. Two games with the same CD Key will get a "SERIAL ALREADY IN USE" error when joining a multiplayer game
-#   2. Changing registry "HKEY_LOCAL_MACHINE\Software\Wow6432Node\Electronic Arts\EA Games\The Battle for Middle-earth\ergc" to fix that gives 3 minute auto-defeat
-# The solution (outlined https://www.gamereplays.org/community/index.php?showtopic=1010428) is to make copies of system.reg and LOTRBFMe.dat and give them to players
-# Since LOTRBFMe.dat is small and we only need a few registry edits, we'll do that instead of full copies
-
 if [[ $# -eq 0 ]] ; then
     echo 'First argument should be Lutris $GAMEDIR (Location containing drive_c, dosdevices, *.reg, etc..)'
     echo 'Exiting...'
